78e191b8df6848e91bf2a67b4d4e12874e4b1d16
Jerry W <jerrywiltse@gmail.com>
2017-06-30 12:14:36 -0400

    .

--- fedora-create-rpm.sh
+++ fedora-create-rpm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 curl https://dl.bintray.com/solvingj/public-rpm/SolvingJ-Public-RPM.repo -o /etc/yum.repos.d/SolvingJ-Public-RPM.repo
+yum install go-bin-rpm
 cd /mnt/travis_build_dir
-yum -y install go-bin-rpm
 go-bin-rpm generate --file rpm-creation-data.json --version 0.0.1
