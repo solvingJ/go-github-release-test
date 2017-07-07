6bbb2d4c8e17fdaacc0e31dee95fbc80ee076910
Jerry W <jerrywiltse@gmail.com>
2017-07-07 14:05:05 -0400

    .

--- ci/travis/04_script.py
+++ ci/travis/04_script.py
@@ -9,19 +9,19 @@ PKG_NAME = REPO_NAME + "-" + ARCH + "-" + PKG_VERSION
 print PKG_NAME
 if PKG_TYPE == "DEB":
   package_cmd=(
-    "go-bin-deb generate " +
-    "--file deb-creation-data.json " +
-    "--version " + PKG_VERSION +
-    "--arch " + ARCH +
-    "-o " + PKG_NAME + ".deb")
+    "go-bin-deb generate" +
+    " --file deb-creation-data.json" +
+    " --version " + PKG_VERSION +
+    " --arch " + ARCH +
+    " -o " + PKG_NAME + ".deb")
   os.system(package_cmd)
   print package_cmd
 elif PKG_TYPE == "RPM":
   package_cmd=(
-    "go-bin-rpm generate " +
-    "--file rpm-creation-data.json " +
-    "--version " + PKG_VERSION +
-    "--arch " + ARCH +
-    "-o " + PKG_NAME + ".rpm")
+    "go-bin-rpm generate" +
+    " --file rpm-creation-data.json" +
+    " --version " + PKG_VERSION +
+    " --arch " + ARCH +
+    " -o " + PKG_NAME + ".rpm")
   os.system("docker run -v " + BUILD_DIR + "/:/mnt/travis solvingj/go-bin-rpm /bin/sh -c " + package_cmd)
  
