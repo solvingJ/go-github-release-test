a9e8a559800596a02e839bc365ef7f528f2882df
Jerry W <jerrywiltse@gmail.com>
2017-07-07 12:39:59 -0400

    .

--- ci/travis/01_before_install.py
+++ ci/travis/01_before_install.py
@@ -1,9 +1,8 @@
 import sys
 import os
 
-os.system("add-apt-repository 'deb https://dl.bintray.com/solvingj/public-deb unstable main'")
-with open("/etc/apt/sources.list") as fin:
-	print fin.read()
+with open("/etc/apt/sources.list", "a") as fin:
+	fin.write("deb https://dl.bintray.com/solvingj/public-deb unstable main")
 
 
  
