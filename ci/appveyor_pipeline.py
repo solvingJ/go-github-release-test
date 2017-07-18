28c6d3685f531e6e104dd2305460f6e1008f0d9d
Jerry W <jerrywiltse@gmail.com>
2017-07-18 13:08:42 -0400

    debug jfrog commands 1210

--- ci/appveyor_pipeline.py
+++ ci/appveyor_pipeline.py
@@ -60,7 +60,7 @@ def create_pkg_location(bt_repo_name):
   
 def package_msi():
   package_cmd=(
-  '"%GO_MSI_PATH%\\go-msi\" make' + 
+  '""%GO_MSI_PATH%\\go-msi\" make"' + 
   " --path msi-creation-data.json" +
   " --version " + PKG_VERSION +
   " --msi " +  PKG_NAME + ".msi")
@@ -71,7 +71,7 @@ def package_nupkg():
   print("Packaging NUPKG")
   
   package_cmd=(
-  '"%GO_MSI_PATH%\\go-msi\" choco' + 
+  '""%GO_MSI_PATH%\\go-msi\" choco"' + 
   " --path msi-creation-data.json" +
   " --version " + PKG_VERSION +
   " --input " + PKG_NAME + ".msi"
