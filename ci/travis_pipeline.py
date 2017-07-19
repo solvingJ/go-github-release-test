b4d112b8e62d976ed3c53c94fbaffe444c6dda68
Jerry W <jerrywiltse@gmail.com>
2017-07-19 10:35:55 -0400

    jfrog working

--- ci/travis_pipeline.py
+++ ci/travis_pipeline.py
@@ -12,7 +12,7 @@ BT_REPO_CONAN = os.environ["BINTRAY_REPO_CONAN"]
 BT_SUBJECT = os.environ["BINTRAY_SUBJECT"]
 BT_USER = os.environ["BINTRAY_USER"]
 BT_KEY = os.environ["BINTRAY_KEY"]
-PKG_VERSION = os.environ["TRAVIS_JOB_NUMBER"] + "." + os.environ["TRAVIS_BUILD_NUMBER"]
+PKG_VERSION = os.environ["TRAVIS_JOB_NUMBER"]
 PKG_PATH_DEB = "unstable/main/" + ARCH + "/"
 PKG_PATH_RPM = GIT_REPO_NAME + "/" + ARCH + "/"
 PKG_PATH_TARGZ = PKG_PATH_RPM
@@ -50,10 +50,10 @@ def package():
   package_conan()
   
 def deploy():
-  print("Downloading JFrog CLI")
-  os.system("curl -fL https://getcli.jfrog.io | sh")
-  print("Configuring JFrog CLI with command: " + "./jfrog bt config --user " + BT_USER + " --key " + BT_KEY + " --licenses MIT")
-  os.system("./jfrog bt config --user " + BT_USER + " --key " + BT_KEY + " --licenses MIT")
+  print("Running install_jfrog_cli()")
+  install_jfrog_cli()
+  print("Running config_jfrog_cli()")
+  config_jfrog_cli()
   
   deb_upload_suffix = "--deb " + PKG_NAME_DEB + " " + create_pkg_location(BT_REPO_DEB) + " " + PKG_PATH_DEB
   rpm_upload_suffix = PKG_NAME_RPM + " " +  create_pkg_location(BT_REPO_RPM) + " " + PKG_PATH_RPM
@@ -96,7 +96,20 @@ def package_targz():
   
 def package_conan():
   print("No instructions for conan packaging yet")
-    
+
+def install_jfrog_cli():
+  install_command=("curl -fL https://getcli.jfrog.io | sh")
+  print("Installing jfrog client with command: " + install_command)
+  os.system(install_command)
+  
+def config_jfrog_cli():
+  configure_command=(
+  "./jfrog bt config --user " + BT_USER + 
+  " --key " + BT_KEY + 
+  " --licenses MIT")
+  print("Configuring jfrog client for bintray uploads with command: " + configure_command)
+  os.system(configure_command)
+  
 def upload_bintray(upload_cmd_suffix):
   upload_cmd_prefix = "./jfrog bt upload --override=true --publish=true "
   print("Uploading files to Bintray with command: " + upload_cmd_prefix + upload_cmd_suffix)
